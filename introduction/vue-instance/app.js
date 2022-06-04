472b31a8d3d8884fbd998a8f4152725e3f145b1e
Tugran Kenger <turankenger@hotmail.com.tr>
2022-06-05 00:14:29 +0300

    vuejs instances

---
+++ introduction/vue-instance/app.js
@@ -0,0 +1,7 @@
+const app = Vue.createApp({
+    data(){ // degiskenleri tanimladigimiz property
+        return {
+            title:"vue practices",
+        };
+    },
+}).mount("#app"); // yukaridaki vuejs uygulamasi id'si app olan element icinde calisacaktir.
