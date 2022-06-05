45d2840a39b312c6c77e760df82f8839fa7cae69
Tugran Kenger <turankenger@hotmail.com.tr>
2022-06-05 16:57:11 +0300

    introduction reactivity

--- introduction/counter-app/app.js
+++ introduction/counter-app/app.js
@@ -6,25 +6,30 @@ const app = Vue.createApp({
         }
     },
     methods:{
-        inc(){
-            this.counter++;
-        },
-        dec(){
-            this.counter--;
-        },
+        
+    },
+    computed:{ // fonksiyon gibi calisan degiskenlerdir (reactivity)
         getCounterResult(){
             console.log('runned counter 1');
             return this.counter > 5 ? 'bigger than 5': 'smaller than 5';
-        },
-        inc2(){
-            this.counter2++;
-        },
-        dec2(){
-            this.counter2--;
         },
         getCounter2Result(){
             console.log('runned counter 2');
             return this.counter2 > 5 ? 'bigger than 5': 'smaller than 5';
         }
     }
+    // methods:{
+    //     inc(){
+    //         this.counter++;
+    //     },
+    //     dec(){
+    //         this.counter--;
+    //     },
+    //     inc2(){
+    //         this.counter2++;
+    //     },
+    //     dec2(){
+    //         this.counter2--;
+    //     },
+    // }
 }).mount("#app")
