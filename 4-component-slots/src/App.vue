be66272c3de6ce92f504dfa1d9d697fcbe9547ff
Tugran Kenger <turankenger@hotmail.com.tr>
2022-06-12 17:44:51 +0300

    vue3 component communication with slot

--- 4-component-slots/src/App.vue
+++ 4-component-slots/src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <ModalDemo :onSave="onSave" :title="'modal title via props'" :content="content" />
+  <!-- <ModalDemo :onSave="onSave" :title="'modal title via props'" :content="content" /> -->
+  <ModalDemo >
+    <template #title>
+      <h3>title via slot</h3>
+    </template>
+    <template  #content>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, incidunt expedita magnam dignissimos unde culpa omnis adipisci? Ullam esse, inventore doloremque odit unde atque maxime harum rerum ducimus at quae.
+    </template>
+
+    <template #default>
+      <span>default value</span>
+    </template>
+  </ModalDemo>
 </template>
 
 <script>
--- 4-component-slots/src/components/ModalDemo.vue
+++ 4-component-slots/src/components/ModalDemo.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="container">
         <div class="header">
-            <h1 class="text-center">{{title}}</h1>
+            <slot name="title"/>
         </div>
-        <div class="content" v-html="content">
-
+        <div class="content">
+            <slot name="content" />
         </div>
+        <hr>
+        <slot />
+        <hr>
         <div class="footer">
             <button @click="onSave" class="btn btn-primary btn-sm me-2">Contact</button>
             <button class="btn btn-primary btn-sm me-2">About Us</button>
