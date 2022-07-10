eb5e7752f9702d36fd7cd252857f3243500ddbd1
Tugran Kenger <tugran.kenger@tsoft.com.tr>
2022-07-10 15:07:45 +0300

    vue global component management (global input)

---
+++ 12-global-input/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import tinput from'./components/T-Input.vue'
+  import { reactive } from 'vue';
+  const state = reactive({
+    inputTest : "",
+    password:"",
+  })
+</script>
+
+<template>
+  <div>
+    <h1>Global input management</h1>
+    <div class="parentInfo">
+      <h3>parent component</h3>
+    text: {{state.inputTest}}
+    password: {{state.password}}
+    <hr>
+    </div><br> <br>
+    <tinput v-model="state.inputTest" placeholder="test input" type="text" name="" /><br>
+    <tinput v-model="state.buttonType" type="button" value="button" name="button" /><br>
+    <tinput v-model="state.password" type="password" />
+  </div>
+</template>
---
+++ 12-global-input/src/components/T-Input.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+const value = ref(props.modelValue)
+
+  const props = defineProps({
+    modelValue: String,
+    placeholder : String,
+    type: String,
+    name: String,
+    value:String,
+  })
+
+  const emit = defineEmits(["update:modelValue"]) // parent tarafindan otomatik yakalanir
+
+  watch(value, (newVal)=>{ //watch ile her degisimi parent component'e aktariyoruz.
+    emit("update:modelValue",newVal)
+  })
+</script>
+
+<template>
+  <div>
+    <input v-model="value" :placeholder="props.placeholder" :type="props.type" :name="props.name" />
+    <span>this is child component(tinput.vue)</span>
+  </div>
+</template>
