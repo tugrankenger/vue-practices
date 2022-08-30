4cdcf90129d51ab9c3bfb7f6719beff9320b071b
tugrankenger <turankenger@hotmail.com.tr>
2022-08-30 22:09:42 +0300

    complete vue dark mode

--- 16-vue-dark-mode/src/components/Toggle.vue
+++ 16-vue-dark-mode/src/components/Toggle.vue
@@ -1,13 +1,21 @@
 <script>
-
+  export default {
+    props: ['mode']
+  }
 </script>
 
 <template>
-  <div>
-    
+  <div class="toggle-wrapper">
+    <label>
+      <span>dark mode:</span>
+      <input 
+      type="checkbox"
+      :checked="(mode === 'dark')? 'checked' : false"
+      @change="$emit('toggle')"
+      >
+    </label>
   </div>
 </template>
 
 <style scoped>
-
 </style>
