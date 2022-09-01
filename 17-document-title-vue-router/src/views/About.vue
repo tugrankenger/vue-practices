c431da9723fcaaa8ad920751dcea8366ece630e3
tugrankenger <turankenger@hotmail.com.tr>
2022-09-02 00:15:20 +0300

    initialize files

---
+++ 17-document-title-vue-router/src/views/About.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <h3>About</h3>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ 17-document-title-vue-router/src/views/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <h3>Home</h3>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
