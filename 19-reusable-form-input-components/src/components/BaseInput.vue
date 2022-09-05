5ab959d047ee3c15d7410ad9133b076739b9c490
tugrankenger <turankenger@hotmail.com.tr>
2022-09-05 22:09:07 +0300

    create reusable input component

---
+++ 19-reusable-form-input-components/src/components/BaseInput.vue
@@ -0,0 +1,30 @@
+<script setup>
+  defineProps({
+    label:{
+      type: [String, Boolean],
+      default: ""
+    },
+    modelValue:{
+      type: String,
+      default: ""
+    }
+  })
+</script>
+
+<template>
+  <div class="input-wrap">
+    <label v-if="label"> {{label}} </label>
+    <input type="text" :value="modelValue" @input ="$emit('update:modelValue',$event.target.value)" v-bind="$attrs">
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .input-wrap{
+    display: flex;
+    flex-direction: column;
+    input{
+      padding: 8px 12px;
+      font-size:16px;
+    }
+  }
+</style>
