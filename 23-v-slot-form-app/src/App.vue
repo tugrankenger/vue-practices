4f4923d2e41917f8902cc4a51b108715eb86402f
tugrankenger <turankenger@hotmail.com.tr>
2022-09-18 15:36:53 +0300

    vue3 named slot form app exercise

--- 23-v-slot-form-app/src/App.vue
+++ 23-v-slot-form-app/src/App.vue
@@ -1,10 +1,39 @@
 <script setup>
-
+  import {reactive} from 'vue'
+import formHelper from './components/formHelper.vue'
+const state = reactive({
+  title: 'i am a dynamic slot title',
+})
 </script>
 
 <template>
   <div>
-    
+    <form-helper :title="'tugrankenger'">
+      <template v-slot:title>
+        <div>
+          {{state.title}}
+        </div>
+      </template>
+      <template v-slot:form-header>
+        <div>
+          <h1>form Header</h1>
+        </div>
+      </template>
+      <template v-slot:form-fields>
+        <div>
+          <h3>form-fields</h3>
+          <input type="text"> &nbsp;
+          <input type="password">
+        </div>
+      </template>
+      <template v-slot:form-controls>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </template>
+      <h2 slot="title">{{state.title}}</h2>
+      <p slot="text">i am the paragraph text for the slot</p>
+    </form-helper>
   </div>
 </template>
 
---
+++ 23-v-slot-form-app/src/components/formHelper.vue
@@ -0,0 +1,60 @@
+<script setup>
+const props = defineProps(
+  {
+    title:{
+      type: String,
+      required: true
+    }
+  }
+)
+</script>
+
+<template>
+  <div>
+    <!-- <slot></slot>
+    <slot name="title"></slot>
+    <h1>i am the form helper</h1>
+    <slot name="text"></slot> -->
+    <!-- <slot></slot> -->
+    <!-- {{title}} -->
+      <div class="title">
+        <slot name="title"></slot>
+      </div>
+    <h1>Please fill out our form..</h1>
+    <form>
+      <div id="form-header">
+        <slot name="form-header"></slot>
+      </div>
+      <div id="form-fields">
+        <slot name="form-fields"></slot>
+      </div>
+      <div id="form-constrols">
+        <slot name="form-controls"></slot>
+      </div>
+      <div id="useful-links">
+        <ul>
+          <li><a href="javascript:void(0)">Link 1</a></li>
+          <li><a href="javascript:void(0)">Link 2</a></li>
+          <li><a href="javascript:void(0)">Link 3</a></li>
+          <li><a href="javascript:void(0)">Link 4</a></li>
+        </ul>
+      </div>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 20px;
+}
+#useful-links ul{
+  display: flex;
+  align-items: center;
+  gap: 30px;
+  flex-wrap: wrap;
+  flex-direction: row;
+}
+</style>
