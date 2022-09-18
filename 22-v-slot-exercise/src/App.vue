081c73995850a8ff743e4970c181c17365fb1196
tugrankenger <turankenger@hotmail.com.tr>
2022-09-18 10:43:58 +0300

    v-slot directive exercise

---
+++ 22-v-slot-exercise/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Btn from './components/Btn.vue';
+
+</script>
+
+<template>
+  <div>
+    <Btn>
+      <template v-slot:content>
+        <span>v-slot exercise</span>
+      </template>
+    </Btn>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ 22-v-slot-exercise/src/components/Btn.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <button class="btn-primary">
+      <slot name="content"></slot>
+    </button>
+  </div>
+</template>
+
+<style scoped>
+button{
+  background: #0069d9;
+  color: #fff;
+}
+</style>
