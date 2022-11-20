c3d48df135ba99a9aa200fff1c559d09ecba8028
tugrankenger <turankenger@hotmail.com.tr>
2022-11-20 21:57:30 +0300

    applied transition type of bounce on element

--- 27-animations-and-transitions/src/App.vue
+++ 27-animations-and-transitions/src/App.vue
@@ -1,31 +1,61 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default{
+  data(){
+    return{
+      show:false
+    }
+  }
+}
 </script>
+
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h3>Animations and transitions</h3>
+    <hr>
+    <button @click="show = !show">Show / Hide</button>
+    <br><br>
+
+    <div class="transition-wrapper">
+      <transition name="bounce">
+      <div class="alert-box" v-if="show">This is a alert box</div>
+    </transition>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+
+.transition-wrapper{
+  width: 100%;
+  height: 100px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+  .alert-box{
+    width: 300px;
+    height: 20px;
+    padding: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid #333;
+    background: aliceblue;
+  }
+
+  .bounce-enter-active {
+  animation: bounce-in 0.5s;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.bounce-leave-active {
+  animation: bounce-in 0.5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.25);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
