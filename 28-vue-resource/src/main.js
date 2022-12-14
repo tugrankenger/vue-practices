0d12c529be5d80a7681e47b23581e9d301a1ca61
tugrankenger <turankenger@hotmail.com.tr>
2022-12-14 22:11:58 +0300

    axios interceptors for request and response

--- 28-vue-resource/src/main.js
+++ 28-vue-resource/src/main.js
@@ -3,5 +3,25 @@ import './style.css'
 import App from './App.vue'
 import Axios from 'axios'
 Axios.defaults.baseURL = "https://vue-resource-f8d71-default-rtdb.firebaseio.com/users.json"
+Axios.interceptors.request.use(function(config){
+  // console.log("config:", config)
+  if(config.method == "get"){
+    alert("this is get request")
+  }else if(config.method == "post"){
+    alert("This is post method")
+  }
+  return config
+},function(error){
+  return Promise.reject(error)
+})
+Axios.interceptors.response.use(function(response){
+  // console.log("response interceptors: ",response)
+  if(response.status == 200){
+    alert("response status is success")
+  }
+  return response
+},function(error){
+  return Promise.reject(error)
+})
 const app = createApp(App)
 app.mount('#app')
