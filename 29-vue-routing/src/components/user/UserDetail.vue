b79131524c03b469781151cdf0c8797bb8066716
tugrankenger <turankenger@hotmail.com.tr>
2022-12-16 22:00:32 +0300

    add hash fragment

--- 29-vue-routing/src/components/user/UserDetail.vue
+++ 29-vue-routing/src/components/user/UserDetail.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="container">
     <h3>User Detail Component</h3>
+    <p>User id: {{$route.params.id}}</p>
+    <router-link 
+      tag="button"
+      class="btn btn-primary"
+      :to="{name:'UserEdit', params:{id:$route.params.id}, query:{name:'tugran', surname:'kenger'}, hash:'#data'}"
+    >Edit user</router-link>
   </div>
 </template>
 
